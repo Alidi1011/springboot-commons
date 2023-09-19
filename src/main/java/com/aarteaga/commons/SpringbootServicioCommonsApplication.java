8c72ddaba1d406309e2256c6de24a475ac11cbf3
Alisson <arteaga10alisson@gmail.com>
2023-09-19 11:20:17 -0500

    add first commit

---
+++ src/main/java/com/aarteaga/commons/SpringbootServicioCommonsApplication.java
@@ -0,0 +1,14 @@
+package com.aarteaga.commons;
+
+//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioCommonsApplication {
+
+	/*
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioCommonsApplication.class, args);
+	}*/
+
+}
